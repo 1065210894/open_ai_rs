9bb60324d19305b31400cef016057acb68680cff
deishy <deishy@qq.com>
2023-02-16 21:43:40 +0800

    接入框架

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,23 @@
-use async_openai::{Client, types::{CreateCompletionRequestArgs}};
+mod handler;
+mod open_ai;
+mod router;
+
+use axum::{
+    extract::Path,
+    routing::{delete, get, post},
+    Router,
+};
 
 #[tokio::main]
 async fn main() {
-    let api_key = "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"; // This secret could be from a file, or environment variable.
-    let client = Client::new().with_api_key(api_key);
+    // build our application with a single route
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }).post(|| {}))
+        .route("/liudosen", delete());
 
-    // Create request using builder pattern
-    let request = CreateCompletionRequestArgs::default()
-        .model("text-davinci-003")
-        .prompt("我插你苦胆这句台词是葛优哪部电影的")
-        .max_tokens(2048_u16)
-        .build()
-        .unwrap();
-
-    // Call API
-    let response = client
-        .completions()      // Get the API "group" (completions, images, etc.) from the client
-        .create(request)    // Make the API call in that "group"
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
         .await
         .unwrap();
-
-    println!("{}", response.choices.first().unwrap().text);
 }
-
-
